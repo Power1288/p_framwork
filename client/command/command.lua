34d7c4f0107198f928d27391efb7a6c7eab263b4
power <laurent5dessy@gmail.com>
2021-11-13 21:44:23 +0100

    add command tpm

--- client/command/command.lua
+++ client/command/command.lua
@@ -3,3 +3,32 @@
 --- Created by ll.
 --- DateTime: 11/11/2021 20:49
 ---
+
+RegisterCommand("tpm", function(source)
+    TeleportToWaypoint()
+end)
+
+TeleportToWaypoint = function()
+    local WaypointHandle = GetFirstBlipInfoId(8)
+    if DoesBlipExist(WaypointHandle) then
+        local waypointCoords = GetBlipInfoIdCoord(WaypointHandle)
+
+        for height = 1, 1000 do
+            SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+
+            local foundGround, zPos = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
+
+            if foundGround then
+                SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+
+                break
+            end
+
+            Citizen.Wait(5)
+        end
+
+        ESX.ShowNotification("Teleported.")
+    else
+        ESX.ShowNotification("Please place your waypoint.")
+    end
+end
